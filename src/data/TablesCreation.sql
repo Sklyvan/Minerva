a5200199d4fa20be31e41f69abcdd41a26403294
Sklyvan <joangraciataribo@gmail.com>
2022-09-01 00:04:55 +0200

    Basic SQLite tables

---
+++ src/data/TablesCreation.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS 'Metadata'
+(
+    CreationDate DATE NOT NULL,
+    LastUpdate DATE NOT NULL,
+    NumberMessages INTEGER NOT NULL,
+    PRIMARY KEY (CreationDate)
+);
+
+CREATE TABLE IF NOT EXISTS 'Messages'
+(
+    ID INTEGER NOT NULL,
+    FromUser INTEGER NOT NULL,
+    ToUser INTEGER NOT NULL,
+    SentDate DATE NOT NULL,
+    ReceivedDate DATE NOT NULL,
+    Content TEXT NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (FromUser) REFERENCES Contacts(ID),
+    FOREIGN KEY (ToUser) REFERENCES Contacts(ID)
+);
+
+CREATE TABLE IF NOT EXISTS 'Contacts'
+(
+    ID INTEGER NOT NULL,
+    UserName TEXT NOT NULL,
+    RSAPublicKey TEXT NOT NULL,
+    CircuitID INTEGER NOT NULL,
+    PRIMARY KEY (ID)
+)
