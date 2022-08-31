6ccd8ca1fb59fa6c26e0a63a6cf9ccdab9b9e517
Sklyvan <joangraciataribo@gmail.com>
2022-08-31 15:46:12 +0200

    Implemented and tested the public user information

---
+++ src/main/PublicUser.py
@@ -0,0 +1,18 @@
+from src.cryp.Imports import RSA
+from src.main.Circuit import Circuit
+
+class PublicUser:
+    def __init__(self, userID: int, userName: str, rsaPublicKeys: [RSA.RsaKey, RSA.RsaKey], throughCircuit: Circuit):
+        self.userID = userID
+        self.userName = userName
+        self.encryptionKey, self.verificationKey = rsaPublicKeys
+        self.throughCircuit = throughCircuit
+
+    def __str__(self):
+        return f"User {self.userName} (ID: {self.userID})"
+
+    def __repr__(self):
+        return f"PublicUser object with user name {self.userName} and ID {self.userID}."
+
+    def __eq__(self, other):
+        return self.userID == other.userID
