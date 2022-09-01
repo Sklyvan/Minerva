6080fea7c4b2b4fdb24d923beafda3230021108b
Sklyvan <joangraciataribo@gmail.com>
2022-09-01 14:30:57 +0200

    SQL Queries to extract a message

---
+++ src/data/GetMessage.sql
@@ -0,0 +1,2 @@
+SELECT * FROM 'Messages' WHERE 'MessageID' = ?; -- Used to get all the stored message information
+SELECT Content FROM 'Messages' WHERE 'MessageID' = ?; -- Used to get just the message content
