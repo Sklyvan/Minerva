d70d00e826dd0247c95a5d92327b9fa917d7a59d
Sklyvan <joangraciataribo@gmail.com>
2022-09-09 13:45:55 +0200

    Fixed problem with ID being a string

--- src/data/GetMessage.sql
+++ src/data/GetMessage.sql
@@ -1,2 +1,2 @@
-SELECT * FROM 'Messages' WHERE 'MessageID' = ?; -- Used to get all the stored message information
-SELECT Content FROM 'Messages' WHERE 'MessageID' = ?; -- Used to get just the message content
+SELECT * FROM 'Messages' WHERE ID = ?;
+SELECT Content FROM 'Messages' WHERE ID = ?;
