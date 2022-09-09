17bcf6143add1854fa5bc980530f1e1cc7b625a0
Sklyvan <joangraciataribo@gmail.com>
2022-09-09 17:08:02 +0200

    Added option for empty received date

--- src/data/InsertMessage.sql
+++ src/data/InsertMessage.sql
@@ -1,2 +1,5 @@
 INSERT INTO 'Messages'(ID, FromUser, ToUser, SentDate, ReceivedDate, Content)
-VALUES (?, ?, ?, ?, ?, ?)
+VALUES (?, ?, ?, ?, ?, ?);
+
+INSERT INTO 'Messages'(ID, FromUser, ToUser, SentDate, Content)
+VALUES (?, ?, ?, ?, ?);
