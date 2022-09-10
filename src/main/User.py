6cda57c71c9d297eaf7a440bd4027bed698c755c
Sklyvan <joangraciataribo@gmail.com>
2022-09-10 22:02:26 +0200

    Export, import and comparison methods

--- src/main/User.py
+++ src/main/User.py
@@ -27,5 +27,44 @@ class User:
     def checkKeys(self):
         return self.encryptionKeys.checkKeys() and self.signingKeys.checkKeys()
 
+    def exportUser(self, path: str):
+        with open(path, "w") as f:
+            f.write(json.dumps(self.asJSON()))
+
+    def importUser(self, path: str):
+        with open(path, "r") as f:
+            data = json.loads(f.read())
+            self.userID = data["userID"]
+            self.userName = data["userName"]
+            self.encryptionKeys = RSAKeys(toImport=True, fileName=data["encryptionKeys"])
+            self.signingKeys = RSAKeys(toImport=True, fileName=data["signingKeys"])
+            self.IP = data["IP"]
+            self.forwardingTable = data["forwardingTable"]
+            self.messagesQueue = data["messagesQueue"]
+            self.userFriends = data["userFriends"]
+            self.messages = MessagesDB(dbPath=data["messages"])
+
+    def asJSON(self):
+        return {"userID": self.userID,
+                "userName": self.userName,
+                "encryptionKeys": self.encryptionKeys.filename,
+                "signingKeys": self.signingKeys.filename,
+                "IP": self.IP,
+                "forwardingTable": 'None', # TODO: Implement forwarding table.
+                "messagesQueue": 'None', # TODO: Implement messages queue.
+                "userFriends": 'None', # TODO: Implement user friends.
+                "messages": self.messages.dbPath}
+
+    def __eq__(self, other):
+        return self.userID == other.userID \
+               and self.userName == other.userName \
+               and self.encryptionKeys == other.encryptionKeys \
+               and self.signingKeys == other.signingKeys \
+               and self.IP == other.IP \
+               and self.forwardingTable == other.forwardingTable \
+               and self.messagesQueue == other.messagesQueue \
+               and self.userFriends == other.userFriends \
+               and self.messages == other.messages
+
     def __str__(self):
-        return f"User {self.userName} (ID: {self.userID})"
+        return f"User {self.userName} (ID: {self.userID}) with IP {self.IP} and keys {self.encryptionKeys} and {self.signingKeys}."
