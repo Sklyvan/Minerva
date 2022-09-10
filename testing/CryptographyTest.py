d92e3f4dcd6a877841fb16386b4763e8c53c6319
Sklyvan <joangraciataribo@gmail.com>
2022-09-10 19:49:45 +0200

    Changed the keys names

--- testing/CryptographyTest.py
+++ testing/CryptographyTest.py
@@ -5,25 +5,25 @@ from src.cryp.RSAKeys import *
 
 class RSATesting(unittest.TestCase):
     def testEncryptionDecryption1024(self):
-        keys = RSAKeys(1024)
+        keys = RSAKeys(1024, fileName='TestKey')
         # Generate a random text
-        text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 100))])
+        text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 50))])
         text = text.encode()
         cipher = keys.encrypt(text, keys.publicKey)
         self.assertEqual(keys.decrypt(cipher), text,
                          "1024 Encryption/Decryption failed.")
 
     def testSigningVerification1024(self):
-        keys = RSAKeys(1024)
+        keys = RSAKeys(1024, fileName='TestKey')
         # Generate a random text
-        text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 100))])
+        text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 50))])
         text = text.encode()
         signature = keys.sign(text)
         self.assertTrue(keys.verify(text, signature, keys.publicKey),
                         "1024 Signing/Verification failed.")
 
     def testEncryptionDecryption2048(self):
-        keys = RSAKeys(2048)
+        keys = RSAKeys(2048, fileName='TestKey')
         # Generate a random text
         text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 100))])
         text = text.encode()
@@ -32,7 +32,7 @@ class RSATesting(unittest.TestCase):
                          "2048 Encryption/Decryption failed.")
 
     def testSigningVerification2048(self):
-        keys = RSAKeys(2048)
+        keys = RSAKeys(2048, fileName='TestKey')
         # Generate a random text
         text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 100))])
         text = text.encode()
@@ -41,7 +41,7 @@ class RSATesting(unittest.TestCase):
                         "2048 Signing/Verification failed.")
 
     def testEncryptionDecryption4096(self):
-        keys = RSAKeys(4096)
+        keys = RSAKeys(4096, fileName='TestKey')
         # Generate a random text
         text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 100))])
         text = text.encode()
@@ -50,7 +50,7 @@ class RSATesting(unittest.TestCase):
                          "4096 Encryption/Decryption failed.")
 
     def testSigningVerification4096(self):
-        keys = RSAKeys(4096)
+        keys = RSAKeys(4096, fileName='TestKey')
         # Generate a random text
         text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 100))])
         text = text.encode()
@@ -59,7 +59,7 @@ class RSATesting(unittest.TestCase):
                         "4096 Signing/Verification failed.")
 
     def testEncryptionDecryption8192(self):
-        keys = RSAKeys(8192)
+        keys = RSAKeys(8192, fileName='TestKey')
         # Generate a random text
         text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 100))])
         text = text.encode()
@@ -68,7 +68,7 @@ class RSATesting(unittest.TestCase):
                          "8192 Encryption/Decryption failed.")
 
     def testSigningVerification8192(self):
-        keys = RSAKeys(8192)
+        keys = RSAKeys(8192, fileName='TestKey')
         # Generate a random text
         text = ''.join([chr(random.randint(0, 255)) for i in range(0, random.randint(0, 100))])
         text = text.encode()
@@ -78,17 +78,13 @@ class RSATesting(unittest.TestCase):
 
 
     def testImportExport(self):
-        Keys1 = RSAKeys()
-        Keys2 = RSAKeys(emptyInstance=True)
+        Keys1 = RSAKeys(fileName='TestKey')
+        Keys2 = RSAKeys(fileName='TestKey', toImport=True)
 
-        Keys1.export("TestKey.pem")
-        Keys2.importKeys("TestKey.pem")
+        Keys1.exportKeys("TestKey")
+        Keys2.importKeys("TestKey")
 
         self.assertTrue(Keys1 == Keys2, "Import/Export failed.")
-
-        # Remove the exported file
-        for file in glob.glob("TestKey.*"):
-            os.remove(file)
 
 
 if __name__ == '__main__':
