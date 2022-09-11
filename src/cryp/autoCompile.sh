ad2c97412637be9f901ebff8d876a980570fc19a
Sklyvan <joangraciataribo@gmail.com>
2022-09-12 00:27:52 +0200

    Improved the compilation process

--- src/cryp/autoCompile.sh
+++ src/cryp/autoCompile.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-# Compile the RSA GoLang script and copy it to the test directory.
-go build GenerateRSA.go
-cp GenerateRSA ../testing/GenerateRSA
+compilationName=${PWD##*/} # Current directory name
+compilationName=${compilationName:-/} # Remove trailing slash
+
+go build .
+# shellcheck disable=SC2181
+if [ $? -eq 0 ]; then
+    echo "RSA script compiled successfully."
+    mv $compilationName RSA
+    cp RSA ../testing/RSA
+else
+    echo "RSA script failed to compile."
+fi
