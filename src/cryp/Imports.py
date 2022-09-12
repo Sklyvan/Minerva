a5d1eae1153ada5f4d3f05e8b82bbbe0025df28b
Sklyvan <joangraciataribo@gmail.com>
2022-09-12 16:41:28 +0200

    Diffie-Hellman required libraries

--- src/cryp/Imports.py
+++ src/cryp/Imports.py
@@ -5,3 +5,6 @@ from Crypto.PublicKey import RSA
 from os import urandom, path, system
 from time import time
 from src.SystemExceptions import *
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
