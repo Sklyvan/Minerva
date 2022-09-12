3eaab47751f904f14b4fc6f97bcb03b23d69560e
Sklyvan <joangraciataribo@gmail.com>
2022-09-12 16:41:04 +0200

    Main Node and Circuit structure

--- src/main/Circuit.py
+++ src/main/Circuit.py
@@ -1,4 +1,83 @@
 from src.SystemExceptions import *
+from src.cryp.DiffieHellmanKey import *
+
+class Node:
+    def __init__(self, nodeIP: str, key: DiffieHellmanKey):
+        self.nodeIP = nodeIP
+        self.key = key
+
+    def isOnline(self) -> bool:
+        raise NotImplementedError("This method is not implemented yet.")
+
+    def checkKey(self) -> bool:
+        raise NotImplementedError("This method is not implemented yet.")
+
+    def exportNode(self, path: str):
+        raise NotImplementedError("This method is not implemented yet.")
+
+    def importNode(self, path: str):
+        raise NotImplementedError("This method is not implemented yet.")
+
+    def __str__(self):
+        return f"Node {self.nodeIP}"
+
+    def __eq__(self, other: "Node"):
+        return self.nodeIP == other.nodeIP
+
 
 class Circuit:
-    None
+    def __init__(self, circuitID: str, nodes: [Node] = []):
+        self.circuitID = circuitID
+        self.nodes = nodes
+        self.size = len(nodes)
+
+    def addNode(self, node: Node):
+        self.nodes.append(node)
+        self.size += 1
+
+    def removeNode(self, node: Node):
+        self.nodes.remove(node)
+        self.size -= 1
+
+    def isOnline(self) -> bool:
+        return all((node.isOnline() for node in self.nodes))
+
+    def checkKeys(self) -> bool:
+        raise NotImplementedError("This method is not implemented yet.")
+
+    def exportCircuit(self, path: str):
+        raise NotImplementedError("This method is not implemented yet.")
+
+    def importCircuit(self, path: str):
+        raise NotImplementedError("This method is not implemented yet.")
+
+    def __len__(self):
+        return self.size
+
+    def __eq__(self, other: "Circuit"):
+        for i in range(len(self)):
+            if self.nodes[i] != other.nodes[i]:
+                return False
+        return True
+
+    def __str__(self):
+        return f"Circuit {self.circuitID} with {self.size} nodes."
+
+    def __iter__(self):
+        return iter(self.nodes)
+
+    def __getitem__(self, index: int):
+        return self.nodes[index]
+
+    def __setitem__(self, index: int, value: Node):
+        self.nodes[index] = value
+
+    def __delitem__(self, index: int):
+        del self.nodes[index]
+        self.size -= 1
+
+    def __contains__(self, item: Node):
+        return item in self.nodes
+
+    def __reversed__(self):
+        return reversed(self.nodes)
