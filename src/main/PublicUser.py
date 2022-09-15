2a51cb9ea3380bd4c0d0e115d983c759880bd905
Sklyvan <joangraciataribo@gmail.com>
2022-09-15 23:29:18 +0200

    Method to export as JSON and compare users

--- src/main/PublicUser.py
+++ src/main/PublicUser.py
@@ -8,6 +8,15 @@ class PublicUser:
         self.encryptionKey, self.verificationKey = rsaPublicKeys
         self.throughCircuit = throughCircuit
 
+    def asJSON(self):
+        return {
+            "UserID": self.userID,
+            "UserName": self.userName,
+            "EncryptionKey": self.encryptionKey.export_key().decode("utf-8"),
+            "VerificationKey": self.verificationKey.export_key().decode("utf-8"),
+            "ThroughCircuit": self.throughCircuit.asJSON()
+        }
+
     def __str__(self):
         return f"User {self.userName} (ID: {self.userID})"
 
@@ -15,4 +24,8 @@ class PublicUser:
         return f"PublicUser object with user name {self.userName} and ID {self.userID}."
 
     def __eq__(self, other: "PublicUser"):
-        return self.userID == other.userID
+        idCheck = self.userID == other.userID
+        nameCheck = self.userName == other.userName
+        encryptionKeyCheck = self.encryptionKey == other.encryptionKey
+        circuitCheck = self.throughCircuit == other.throughCircuit
+        return idCheck and nameCheck and encryptionKeyCheck and circuitCheck
