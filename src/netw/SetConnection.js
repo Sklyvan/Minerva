afd2742f1e387beade47b97c671767f518871858
Sklyvan <joangraciataribo@gmail.com>
2022-09-25 21:16:11 +0200

    Function to read the WebSocket of NodeJS data

--- src/netw/SetConnection.js
+++ src/netw/SetConnection.js
@@ -1,6 +1,29 @@
-const myPeer = new Peer('');
-var isReady = false;
+const myPeer = new Peer('d8cd7bae-0b80-4a71-a7e5-22f728016311');
 var p2pConnection = null;
+var isReady = false;
+
+var xml = new XMLHttpRequest();
+xml.open('GET', 'SocketsInformation.xml', false);
+xml.send();
+var HOST = xml.responseXML.getElementsByTagName('hostJstoHTML')[0].childNodes[0].nodeValue;
+var PORT = xml.responseXML.getElementsByTagName('portJstoHTML')[0].childNodes[0].nodeValue;
+
+function socketOpenReadClose()
+{
+    var ws = new WebSocket('ws://' + HOST + ':' + PORT);
+    ws.onopen = function (event)
+    {
+        ws.onmessage = function (event)
+        {
+            let data = JSON.parse(event.data);
+            connectAndSend(data['toIP'], data['Data']);
+        };
+    };
+    setTimeout(function ()
+    {
+        ws.close();
+    }, 750);
+}
 
 myPeer.on('open', peerID =>
 {
@@ -16,15 +39,17 @@ myPeer.on('connection', conn =>
     });
 });
 
-function connectToPeer(peerID)
+function connectAndSend(peerID, data)
 {
     if (!isReady)
     {
-        setTimeout(() => connectToPeer(peerID), 500);
+        setTimeout(() => connectAndSend(peerID, data), 500);
     }
     p2pConnection = myPeer.connect(peerID);
     p2pConnection.on('open', () =>
     {
-        p2pConnection.send("Hello!");
+        p2pConnection.send(data);
     });
 }
+
+setInterval(socketOpenReadClose, 1000);
