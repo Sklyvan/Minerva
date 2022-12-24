4e51f1520a517e64280cc6a48e792e1bf3ca9f54
Sklyvan <joangraciataribo@gmail.com>
2022-12-24 19:45:57 +0100

    Creation of the function to create or load a user instance
    Took 9 minutes

---
+++ src/main/InitializeUser.py
@@ -0,0 +1,44 @@
+import sys, os, time
+from src.main.User import User
+
+def initializeUser(sysArgs: list) -> User:
+    loadUser = False
+    if len(sysArgs) == 3:
+        if sysArgs[1] == '-l':
+            loadUser = True
+            fileName = sysArgs[2]
+            if not os.path.exists(fileName):
+                print("File does not exist")
+                sys.exit(1)
+            if not fileName.endswith('.json'):
+                print("File must be a json file")
+                sys.exit(1)
+        elif sysArgs[1] == '-n':
+            userName = sysArgs[2]
+        else:
+            print("Invalid command line arguments")
+            sys.exit(1)
+    elif len(sysArgs) != 1:
+        print("Invalid command line arguments")
+        sys.exit(1)
+
+    if loadUser:
+        myUser = User(None, None, [None, None], None)
+        myUser.importUser(fileName)
+    else:
+        userID = int(os.urandom(16).hex(), 16)
+        networkID = [os.urandom(4).hex(),
+                     os.urandom(2).hex(),
+                     os.urandom(2).hex(),
+                     os.urandom(2).hex(),
+                     os.urandom(6).hex()]
+        networkID = '-'.join(networkID)
+        # TODO: This creation of the userID and networkID is temporary.
+
+        t0 = int(time.time())
+        myUser = User(userID, userName,
+                      [RSAKeys(fileName=f'{userName}_EncKeys_{t0}'), RSAKeys(fileName=f'{userName}_SigKeys_{t0}')],
+                      networkID, ForwardingTable(), Queue(), Contacts(), MessagesDB())
+        myUser.exportUser(f'User_{userName}.json')
+        
+    return myUser
