17521e926812aa74bc1a4eed947e4ba3f3290360
Sklyvan <joangraciataribo@gmail.com>
2022-12-25 02:01:38 +0100

    Launch the listener as a unique thread
    Took 41 minutes

--- src/main/App.py
+++ src/main/App.py
@@ -1,4 +1,5 @@
 import subprocess
+import threading
 import sys
 
 from src.main.InitializeUser import initializeUser
@@ -7,4 +8,7 @@ from src.netw.p2p.MessagesListener import start
 if __name__ == '__main__':
     myUser = initializeUser(sys.argv)
     myPipe = subprocess.Popen(["cat"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    start(myPipe)
+
+    threading.Thread(target=start, args=(myPipe,)).start()
+
+    print("Hello!")
