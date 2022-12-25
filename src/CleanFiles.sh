a5def5ccfcb5e913f9af1806ecc560e84f8b0437
Sklyvan <joangraciataribo@gmail.com>
2022-12-25 01:20:13 +0100

    Delete also the JSON files with user information
    Took 2 minutes

--- src/CleanFiles.sh
+++ src/CleanFiles.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Remove all the files ending in .pem or .db from the current directory and all subdirectories
-
 find . -name "*.pem" -exec rm -f {} \;
 find . -name "*.db" -exec rm -f {} \;
+
+# Remove also all the files matching User_*.json
+find . -name "User_*.json" -exec rm -f {} \;
