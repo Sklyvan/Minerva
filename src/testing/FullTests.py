c9957baa75825f12446a2db94d49943f74ece2a6
Sklyvan <joangraciataribo@gmail.com>
2022-12-27 00:13:06 +0100

    Do not test keys bigger than 2048
    Took 2 minutes

--- src/testing/FullTests.py
+++ src/testing/FullTests.py
@@ -753,52 +753,6 @@ class RSATesting(unittest.TestCase):
         for file in glob.glob("TestKey*"):
             os.remove(file)
 
-    def testEncryptionDecryption4096(self):
-        keys = RSAKeys(4096, fileName="TestKey")
-        # Generate a random text
-        text = ("".join([chr(random.randint(32, 126)) for _ in range(1000)])).encode()
-        cipher = keys.encrypt(text, keys.publicKey)
-        self.assertEqual(
-            keys.decrypt(cipher), text, "4096 Encryption/Decryption failed."
-        )
-        for file in glob.glob("TestKey*"):
-            os.remove(file)
-
-    def testSigningVerification4096(self):
-        keys = RSAKeys(4096, fileName="TestKey")
-        # Generate a random text
-        text = ("".join([chr(random.randint(32, 126)) for _ in range(1000)])).encode()
-        signature = keys.sign(text)
-        self.assertTrue(
-            keys.verify(text, signature, keys.publicKey),
-            "4096 Signing/Verification failed.",
-        )
-        for file in glob.glob("TestKey*"):
-            os.remove(file)
-
-    def testEncryptionDecryption8192(self):
-        keys = RSAKeys(8192, fileName="TestKey")
-        # Generate a random text
-        text = ("".join([chr(random.randint(32, 126)) for _ in range(1000)])).encode()
-        cipher = keys.encrypt(text, keys.publicKey)
-        self.assertEqual(
-            keys.decrypt(cipher), text, "8192 Encryption/Decryption failed."
-        )
-        for file in glob.glob("TestKey*"):
-            os.remove(file)
-
-    def testSigningVerification8192(self):
-        keys = RSAKeys(8192, fileName="TestKey")
-        # Generate a random text
-        text = ("".join([chr(random.randint(32, 126)) for _ in range(1000)])).encode()
-        signature = keys.sign(text)
-        self.assertTrue(
-            keys.verify(text, signature, keys.publicKey),
-            "8192 Signing/Verification failed.",
-        )
-        for file in glob.glob("TestKey*"):
-            os.remove(file)
-
     def testImportExport(self):
         Keys1 = RSAKeys(fileName="TestKey")
         Keys2 = RSAKeys(fileName="TestKey", toImport=True)
