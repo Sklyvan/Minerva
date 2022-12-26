976c3b53ae3a471449cc8497b235327a3ab02b6c
Sklyvan <joangraciataribo@gmail.com>
2022-12-27 00:26:49 +0100

    Read from the pipe using async
    Took 13 minutes

--- src/main/App.py
+++ src/main/App.py
@@ -1,6 +1,7 @@
+import asyncio
 import subprocess
-import threading
 import sys
+import threading
 
 from src.main.InitializeUser import initializeUser
 from src.netw.p2p.MessagesListener import start
@@ -10,3 +11,10 @@ if __name__ == "__main__":
     myPipe = subprocess.Popen(["cat"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
     threading.Thread(target=start, args=(myPipe,)).start()
+
+    async def _readPipe():
+        while True:
+            inputPacket = myPipe.stdout.readline()
+            print(inputPacket)
+
+    asyncio.run(_readPipe())
