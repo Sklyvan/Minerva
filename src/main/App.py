453eddf4bd8866d2212223b267f8357de51e4177
Sklyvan <joangraciataribo@gmail.com>
2022-12-28 14:37:47 +0100

    Store the thread, then start the thread
    Took 24 seconds

--- src/main/App.py
+++ src/main/App.py
@@ -1,5 +1,5 @@
 import subprocess
-import sys
+import sys, os
 import threading
 
 from src.main.InitializeUser import initializeUser
@@ -7,6 +7,7 @@ from src.netw.p2p.MessagesListener import messagesListener
 
 emojiTick, emojiCross = "\u2705", "\u274C"
 comPipe = subprocess.Popen(["cat"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+threads = []
 
 
 def readPipe():
@@ -17,6 +18,11 @@ def readPipe():
 
 
 if __name__ == "__main__":
+    # Run the $ node src/netw/openWebSocket.js command as a thread.
+    t = threading.Thread(target=os.system, args=("node ../netw/openWebSocket.js",))
+    t.start()
+    threads.append(t)
+
     try:
         myUser = initializeUser(sys.argv)
     except Exception as e:
@@ -26,7 +32,9 @@ if __name__ == "__main__":
         print(f"[{emojiTick}] User Initialized: {myUser.userName} | {myUser.IP}")
 
     try:
-        threading.Thread(target=messagesListener, args=(comPipe,)).start()
+        t = threading.Thread(target=messagesListener, args=(comPipe,))
+        t.start()
+        threads.append(t)
     except Exception as e:
         print(f"[{emojiCross}] {e}")
         sys.exit(1)
@@ -34,9 +42,20 @@ if __name__ == "__main__":
         print(f"[{emojiTick}] Messages Listener Started")
 
     try:
-        threading.Thread(target=readPipe).start()
+        t = threading.Thread(target=readPipe)
+        t.start()
+        threads.append(t)
     except Exception as e:
         print(f"[{emojiCross}] {e}")
         sys.exit(1)
     else:
         print(f"[{emojiTick}] Pipe Reader Started")
+
+    # Wait for all the threads, if KeyboardInterrupt is pressed, then exit.
+    try:
+        for t in threads:
+            t.join()
+    except KeyboardInterrupt:
+        for t in threads[::-1]:
+            t.exit()
+        sys.exit(0)
