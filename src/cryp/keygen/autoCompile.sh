6a4e2df22a87dc880b6f23f9c1951ce5a8a25d90
Sklyvan <joangraciataribo@gmail.com>
2022-12-29 00:23:08 +0100

    The RSA binary now has a .BIN extension
    Took 2 hours 53 minutes

--- src/cryp/keygen/autoCompile.sh
+++ src/cryp/keygen/autoCompile.sh
@@ -7,9 +7,9 @@ go build .
 # shellcheck disable=SC2181
 if [ $? -eq 0 ]; then
     echo "RSA script compiled successfully."
-    mv $compilationName RSA # Change the file name to RSA
-    cp RSA ../../testing/RSA # Copy the file to the testing directory
-    mv RSA .. # Move out the file from the GoLang directory
+    mv $compilationName RSA.bin # Change the file name to RSA
+    cp RSA.bin ../../testing/RSA.bin # Copy the file to the testing directory
+    mv RSA.bin .. # Move out the file from the GoLang directory
 else
     echo "RSA script failed to compile."
 fi
