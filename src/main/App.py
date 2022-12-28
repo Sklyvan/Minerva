267ea791947a1d0d0d01f7c99631ab48a5aab18c
Sklyvan <joangraciataribo@gmail.com>
2022-12-28 13:03:55 +0100

    Run everything with threads instead of AsyncIO
    Took 37 seconds

--- src/main/App.py
+++ src/main/App.py
@@ -1,20 +1,41 @@
-import asyncio
 import subprocess
 import sys
 import threading
 
 from src.main.InitializeUser import initializeUser
-from src.netw.p2p.MessagesListener import start
+from src.netw.p2p.MessagesListener import messagesListener
+
+emojiTick, emojiCross = "\u2705", "\u274C"
+comPipe = subprocess.Popen(["cat"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+
+def readPipe():
+    while True:
+        inputPacket = comPipe.stdout.readline()
+        print(inputPacket)
+
 
 if __name__ == "__main__":
-    myUser = initializeUser(sys.argv)
-    myPipe = subprocess.Popen(["cat"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    try:
+        myUser = initializeUser(sys.argv)
+    except Exception as e:
+        print(f"[{emojiCross}] {e}")
+        sys.exit(1)
+    else:
+        print(f"[{emojiTick}] User Initialized")
 
-    threading.Thread(target=start, args=(myPipe,)).start()
+    try:
+        threading.Thread(target=messagesListener, args=(comPipe,)).start()
+    except Exception as e:
+        print(f"[{emojiCross}] {e}")
+        sys.exit(1)
+    else:
+        print(f"[{emojiTick}] Messages Listener Started")
 
-    async def _readPipe():
-        while True:
-            inputPacket = myPipe.stdout.readline()
-            print(inputPacket)
-
-    asyncio.run(_readPipe())
+    try:
+        threading.Thread(target=readPipe).start()
+    except Exception as e:
+        print(f"[{emojiCross}] {e}")
+        sys.exit(1)
+    else:
+        print(f"[{emojiTick}] Pipe Reader Started")
