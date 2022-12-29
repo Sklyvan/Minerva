03ea82b10e3a116cfdb871ecd34263169432193a
Sklyvan <joangraciataribo@gmail.com>
2022-12-29 01:39:41 +0100

    Blank line at the end
    Took 6 minutes

--- src/CleanFiles.sh
+++ src/CleanFiles.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Remove all the files ending in .pem or .db from the current directory and all subdirectories
+# Remove all the files ending in .pem or .db from the current directory
+# and all subdirectories
 find . -name "*.pem" -exec rm -f {} \;
 find . -name "*.db" -exec rm -f {} \;
 
