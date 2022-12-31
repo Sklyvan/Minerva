3206eeeb5b9df9d82140c503f59e0d9a49ddcf5d
Sklyvan <joangraciataribo@gmail.com>
2022-12-31 21:00:08 +0100

    Display warning message about what the script does
    Took 3 minutes

--- src/CleanFiles.sh
+++ src/CleanFiles.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Warning: Running this script will delete all user data and settings."
+sleep 3
+
 # Remove all the files ending in .pem or .db from the current directory
 # and all subdirectories
 find . -name "*.pem" -exec rm -f {} \;
