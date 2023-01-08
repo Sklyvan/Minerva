0db55ca0efd677e3b251b9bc2f8b04b83f84c7b9
Sklyvan <joangraciataribo@gmail.com>
2023-01-08 13:46:10 +0100

    Generate the UserID and the NetworkID on separate functions
    Took 16 minutes

--- src/main/InitializeUser.py
+++ src/main/InitializeUser.py
@@ -8,6 +8,22 @@ If the argument -n is used, a new user will be created with the username <userna
 """
 
 
+def createUserID():
+    return random.randint(0, 1000)
+
+
+def createNetworkID():
+    networkID = [
+        os.urandom(4).hex(),
+        os.urandom(2).hex(),
+        os.urandom(2).hex(),
+        os.urandom(2).hex(),
+        os.urandom(6).hex(),
+    ]
+    networkID = "-".join(networkID)
+    return networkID
+
+
 def initializeUser(sysArgs: list) -> (User, str):
     """
     This file is called with the arguments -l <filename> or -n <username>.
@@ -18,7 +34,7 @@ def initializeUser(sysArgs: list) -> (User, str):
     """
     loadUser = False
     if len(sysArgs) == 3:
-        if sysArgs[1] == "-l":
+        if sysArgs[1] == "-l":  # We want to load a user from a JSON file.
             loadUser = True
             fileName = sysArgs[2]
             filePath = os.path.join(USER_PATH, fileName)
@@ -28,7 +44,7 @@ def initializeUser(sysArgs: list) -> (User, str):
             if not filePath.endswith(USERFILE_EXTENSION):
                 print(f"File must be a {USERFILE_EXTENSION.upper()} file")
                 sys.exit(1)
-        elif sysArgs[1] == "-n":
+        elif sysArgs[1] == "-n":  # We want to create a new user.
             userName = sysArgs[2]
         else:
             print("Invalid command line arguments")
@@ -41,18 +57,10 @@ def initializeUser(sysArgs: list) -> (User, str):
         myUser = User(None, None, [None, None], None)
         myUser.importUser(filePath)
     else:
-        networkID = [
-            os.urandom(4).hex(),
-            os.urandom(2).hex(),
-            os.urandom(2).hex(),
-            os.urandom(2).hex(),
-            os.urandom(6).hex(),
-        ]
-        networkID = "-".join(networkID)
-
-        userID = random.randint(0, 1000)
-        # TODO: This creation of the userID and networkID is temporary.
-
+        userID, networkID = (
+            createUserID(),
+            createNetworkID(),
+        )  # TODO: This creations of the ID are temporary.
         myUser = createUser(userName, networkID, userID)
         exportName = USERFILE_NAME + userName + "." + USERFILE_EXTENSION
         myUser.exportUser(os.path.join(USER_PATH, exportName))
